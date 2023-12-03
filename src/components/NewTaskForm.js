86bd6a485139133af64ed00d87af02e8d2a2ada9
Benson Mwangi <kimaniben@gmail.com>
2023-12-03 09:18:40 -0500

    Completed assignment

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ onTaskFormSubmit }) {
+  const [formData, setFormData] = useState({
+    text: "",
+    category: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.text && formData.category) {
+      // Only submit the form if text and category are provided
+      onTaskFormSubmit(formData);
+      // Clear the form data after submission
+      setFormData({ text: "", category: "" });
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          value={formData.text}
+          onChange={handleInputChange}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleInputChange}
+        >
+          <option value="">Select Category</option>
+          <option value="Code">Code</option>
+          <option value="Food">Food</option>
+          <option value="Money">Money</option>
+          <option value="Misc">Misc</option>
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
-
-function TaskList() {
+import Task from "./Task"; // assuming Task component is in the same directory
+function TaskList({ tasks, onDeleteTask }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, index) => (
+        <Task
+          key={`${task.category}-${task.text}-${index}`}
+          category={task.category}
+          text={task.text}
+          onDelete={() => onDeleteTask(task.id)}
+        />
+      ))}
     </div>
   );
 }
